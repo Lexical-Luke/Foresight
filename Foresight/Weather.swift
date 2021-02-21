80bd9dcffd284ae93a2fe289f3ecfb5a39b3dedd
Luke Wilkinson <petetheodddog@gmail.com>
2021-02-21 18:22:21 +0200

    Will this commit?

---
+++ Foresight/Weather.swift
@@ -0,0 +1,22 @@
+//
+//  Weather.swift
+//  Foresight
+//
+//  Created by Luke Wilkinson on 2021/02/21.
+//
+
+import Foundation
+
+public struct Weather{
+	let city: String
+	let temperature: String
+	let description: String
+	let iconName: String
+	
+	init(response: APIResponse) {
+		city = response.name
+		temperature = "\(Int(response.main.temp))"
+		description = response.weather.first?.description ?? ""
+		iconName = response.weather.first?.iconName ?? ""
+	}
+}
---
+++ Foresight/WeatherView.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherView.swift
+//  Foresight
+//
+//  Created by Luke Wilkinson on 2021/02/21.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+	
+	@ObservedObject var viewModel: WeatherViewModel
+	
+    var body: some View {
+		VStack {
+			Text(viewModel.cityName)
+				.font(.largeTitle)
+				.padding()
+			Text(viewModel.temperature)
+				.font(.system(size: 70))
+				.bold()
+			Text(viewModel.weatherIcon)
+				.font(.largeTitle)
+				.padding()
+			Text(viewModel.weatherDescription)
+		}.onAppear(perform: viewModel.refresh)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(viewModel: WeatherViewModel(weatherService: WeatherService()))
+    }
+}
